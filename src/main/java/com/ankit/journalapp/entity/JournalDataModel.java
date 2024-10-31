667ba5fbc2eecf5fa8befd9e402c33385af44540
Ankit Kumar <93072882+ankitkumar0112@users.noreply.github.com>
2024-10-31 22:58:56 +0530

    Created a user table

--- src/main/java/com/ankit/journalapp/entity/JournalDataModel.java
+++ src/main/java/com/ankit/journalapp/entity/JournalDataModel.java
@@ -1,9 +1,6 @@
 package com.ankit.journalapp.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,11 +8,13 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
-@Entity(name = "journal")
+@Entity
+@Table(name = "journal_entry", indexes = @Index(columnList = "title"))
 public class JournalDataModel {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @Column(name = "title", nullable = false)
     private String title;
     private String content;
     private LocalDateTime dateTime;
